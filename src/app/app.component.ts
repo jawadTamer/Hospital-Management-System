7283aff3b5c55967081ac529997a3f0ffcb11c28
jawad <jawad@.com>
2025-04-09 04:53:34 +0200

    refactor(app): remove unused SidebarComponent import and update angular.json
    Remove the unused SidebarComponent import in app.component.ts to clean up the code. Update angular.json to include allowedCommonJsDependencies, stylePreprocessorOptions, and preserveSymlinks for better build configuration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './component/dashboard/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent]
+  imports: [RouterOutlet]  // Remove SidebarComponent since it's not used in the template
 })
 export class AppComponent {
   title = 'Hospital-Management-System';
